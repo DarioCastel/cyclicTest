b2c223effa856dd436467112cbab80e63bd1c8d6
DarioCastel <lecastelnovo@gmail.com>
2023-08-04 19:33:46 -0300

    newIndexconnectMongo

--- connectDB.js
+++ connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async ()=>{
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect("mongodb+srv://Motor:tqYBKphbcxidC22O@cluster0.7whtdl1.mongodb.net/Note");
         console.log(`DataBase connected ${conn.connection.host}`);
     } catch (error) {
         console.log(error)
