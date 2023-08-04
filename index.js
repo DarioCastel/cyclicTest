dde636980c9f25681066aa1cecbf0fa6465e91c7
DarioCastel <lecastelnovo@gmail.com>
2023-08-04 19:15:47 -0300

    newIndex2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
+
+const cors = require("cors");
 const express = require("express");
+
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT | 5000;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json({message:"oli"})
-})
 
-app.listen(PORT,()=>{
-    console.log(`server started on ${PORT}`)
-})
+app.get("/", (req, res) => {
+  res.json("Mi 1era api");
+});
+
+app.get("*", (req, res) => {
+  res.sendStatus("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runnig on port: ${PORT}`);
+});
