227a3bc897514ea233056f5840528491c732e138
DarioCastel <lecastelnovo@gmail.com>
2023-08-04 20:13:04 -0300

    noteTest3

--- Routes/api.js
+++ Routes/api.js
@@ -1,17 +1,9 @@
 const router = require('express').Router();
 
-const apiNotes = require('./Notes')
-const apiPost = require('./Posts')
-const apiUser = require('./Login')
+const apiNotes = require('./Notes');
 
 
 //rutas de Notas
-router.use('/notes',apiNotes)
-
-//rutas de Post
-router.use('/post',apiPost)
-
-//rutas de User
-router.use('/user',apiUser)
+router.use('/notes',apiNotes);
 
 module.exports = router
