8baffd26ad6dd878a5e06ccc9b4175e859d565c6
DarioCastel <lecastelnovo@gmail.com>
2023-08-04 19:17:31 -0300

    newIndex3

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
 
-const cors = require("cors");
 const express = require("express");
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 
 app.get("/", (req, res) => {
