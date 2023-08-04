7b959455e50fcc1003f9a65b4659a13aba72b8d5
DarioCastel <lecastelnovo@gmail.com>
2023-08-04 20:10:14 -0300

    noteTest2

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const apiRouter = require("./Routes/api")
 const PORT = process.env.PORT | 5000;
 
 connectDB();
-app.use("/api",apiRouter)
 app.get("/",(req,res)=>{
     res.json({message:"oli"})
 })
+app.use("/api",apiRouter)
 
 app.listen(PORT,()=>{
     console.log(`server started on ${PORT}`)
